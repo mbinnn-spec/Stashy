11ae03eaffb45dc9349cca3472acdf9a83dc5138
Anggara <kusumabintang18@gmail.com>
2026-09-22 14:04:31 +0700

    build: configure release signing and keystore gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,9 +22,19 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("stashy-release.jks")
+            storePassword = "stashypass"
+            keyAlias = "stashy"
+            keyPassword = "stashypass"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
